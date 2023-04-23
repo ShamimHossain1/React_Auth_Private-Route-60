b41703e62be0b7d865410346131b10e45af1520a
ShamimHossain1 <beastb697@gmail.com>
2023-04-24 01:23:02 +0600

    conditional rendaring

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -17,11 +17,15 @@ const Header = () => {
             <div className="navbar bg-primary text-primary-content flex justify-between lg:px-28 px-3">
                 <p className="lg:text-2xl font-bold">Auth Master</p>
                 <div className='lg:pr-52'>
-                    <Link to='/'         className="btn btn-ghost normal-case lg:text-xl">Home    </Link>
-                    <Link to='/orders'   className="btn btn-ghost normal-case lg:text-xl">Orders  </Link>
-                    <Link to='/profile'   className="btn btn-ghost normal-case lg:text-xl">Profile  </Link>
-                    <Link to='/login'    className="btn btn-ghost normal-case lg:text-xl">Login   </Link>
+                    <Link to='/' className="btn btn-ghost normal-case lg:text-xl">Home    </Link>
+                    {user && <Link to='/orders' className="btn btn-ghost normal-case lg:text-xl">Orders  </Link>}
+                    {user && <Link to='/profile' className="btn btn-ghost normal-case lg:text-xl">Profile  </Link>}
+
+
+                    <Link to='/login' className="btn btn-ghost normal-case lg:text-xl">Login</Link>
                     <Link to='/register' className="btn btn-ghost normal-case lg:text-xl">Register</Link>
+
+
                 </div>
                 {
                     user ? <div>{user.email} <Button onClick={handleLogout}>Sign Out</Button></div> : <Link to='/login'><Button>Sign In</Button></Link>
